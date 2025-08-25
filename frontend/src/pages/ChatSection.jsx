324562c3c1d4c78ee39c94bf5db8c4735dc928c2
Varad Pandey <novapro234yt@gmail.com>
2025-08-25 19:29:33 +0530

    added feature to send message on frontend

--- frontend/src/pages/ChatSection.jsx
+++ frontend/src/pages/ChatSection.jsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../api/axios.js";
-import { UserMessage } from "../components/UserMessage.jsx";
+import { UserMessage } from "../components/messages/UserMessage.jsx";
 import { useAuth } from "../context/AuthContext.jsx";
-import { OtherUserMessage } from "../components/OtherUserMessage.jsx";
+import { OtherUserMessage } from "../components/messages/OtherUserMessage.jsx";
 
 export function ChatSection(){
     const {chatId}=useParams();
     const [messages,setMessages]=useState([]);
     const [loading,setLoading]=useState(true);
     const {user}=useAuth();
+
+    const [newContent,setNewContent]=useState({
+        content: "",
+        messageType: "text",
+        chatId
+    });
 
     useEffect(()=>{
         const fetchMessages=async()=>{
@@ -26,12 +32,30 @@ export function ChatSection(){
             }
         }
         fetchMessages();
-    },[chatId]);
+    },[]);
+
+    const handleChange=(event)=>{
+        setNewContent(prev=>({...prev,[event.target.name]:event.target.value}))
+    }
+
+    const handleSubmitContent=async(event)=>{
+        event.preventDefault();
+        if(!newContent.content.trim()) return;
+
+        try{
+            const res=await api.post('/message/createMessage',newContent);
+            setMessages(prev=>[...prev,res.data.data]);
+            setNewContent({content: "",messageType: "text",chatId});
+            console.log(res.data.data);
+        }catch(error){
+            console.log('CATCH BLOCK ERROR: ',error);
+        }
+    }
 
     if (loading) return <p>Loading Chat Messages....</p>
 
     return(
-        <div>
+        <>
             {
                 messages.map((message,index)=>{
                     return(
@@ -41,6 +65,23 @@ export function ChatSection(){
                     )
                 })
             }
-        </div>
+
+            <div>
+                <input type="text"
+                    placeholder="Type Message Here..."
+                    name="content"
+                    value={newContent.content}
+                    onChange={handleChange}
+                    className="border border-gray-400 rounded-md p-2 mb-2 bg-gray-50 mr-5"
+                /> 
+                
+                <button
+                    onClick={handleSubmitContent}
+                    className="border border-gray-400 rounded-md p-2 mb-2 bg-purple-100"
+                >
+                    Send
+                </button> 
+            </div>
+        </>
     )
 }
