17fcfcabfa93af3cb8cf846b19c3d26366547020
Varad Pandey <novapro234yt@gmail.com>
2025-10-14 21:21:31 +0530

    backend deployed fix 2

--- backend/src/db/mongodb.js
+++ backend/src/db/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb=async()=>{
     try{
         const localURI='mongodb://localhost:27017/chat';
-        const mongoURI=(process.env.NODE_ENV==='prodction')?process.env.MONGO_URI_ATLAS:localURI;
+        const mongoURI=(process.env.NODE_ENV==='production')?process.env.MONGO_URI_ATLAS:localURI;
 
         const connectionInstance=await mongoose.connect(mongoURI);
         console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
